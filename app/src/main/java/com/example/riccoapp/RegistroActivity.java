af4fda4451752d3928858f9d0bf5cd708026e0bc
carla <arevalocarlaelizabeth@gmail.com>
2024-09-17 17:32:05 -0300

    Activity perfil de usuario

--- app/src/main/java/com/example/riccoapp/RegistroActivity.java
+++ app/src/main/java/com/example/riccoapp/RegistroActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class registro extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity {
 
 
 
