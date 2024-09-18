894c04b81a4ceadd878f5dd644cb94d1972db3cf
Noobuyer <cevascoagustin@gmail.com>
2024-09-18 16:36:14 -0300

    splash activity

--- app/src/main/java/com/example/riccoapp/SplashActivity.java
+++ app/src/main/java/com/example/riccoapp/SplashActivity.java
@@ -1,24 +1,29 @@
 package com.example.riccoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // Establece el layout de la pantalla de precarga
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Usamos un Handler para retrasar la apertura de la MainActivity por unos segundos
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Luego de 3 segundos, abrir la actividad principal
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                // Finalizar SplashActivity para evitar volver a ella
+                finish();
+            }
+        }, 3000); // Duración de 3 segundos para la pantalla de precarga
     }
 }
