b16d606a120ad111044e2a822c7fa41f4d916d09
LAURA PATRICIA CRUZ <95201770+LauraPatriciaCruz@users.noreply.github.com>
2024-10-12 00:33:39 -0300

    Actualicé archivos de administrador de productos y borré la ventana de diálogo de edición y borrado

--- app/src/main/java/com/example/riccoapp/ProductoViewModelActivity.java
+++ app/src/main/java/com/example/riccoapp/ProductoViewModelActivity.java
@@ -3,32 +3,26 @@ package com.example.riccoapp;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 import java.util.ArrayList;
 import java.util.List;
 
-// Clase ViewModel para manejar los productos
 public class ProductoViewModelActivity extends ViewModel {
-    // LiveData que almacena la lista de productos
-    private MutableLiveData<List<Producto>> listaProductos = new MutableLiveData<>(new ArrayList<>());
+    private MutableLiveData<List<ProductoActivity>> listaProductos = new MutableLiveData<>(new ArrayList<>());
 
-    // Método para obtener la lista de productos
-    public LiveData<List<Producto>> obtenerProductos() {
+    public LiveData<List<ProductoActivity>> obtenerProductos() {
         return listaProductos;
     }
 
-    // Método para agregar un producto a la lista
-    public void agregarProducto(Producto producto) {
-        List<Producto> productosActuales = listaProductos.getValue();
+    public void agregarProducto(ProductoActivity producto) {
+        List<ProductoActivity> productosActuales = listaProductos.getValue();
         productosActuales.add(producto);
-        listaProductos.setValue(productosActuales); // Notifica que la lista ha cambiado
+        listaProductos.setValue(productosActuales);
     }
 
-    // Método para actualizar un producto en una posición específica
-    public void actualizarProducto(int position, Producto productoActualizado) {
-        List<Producto> productosActuales = listaProductos.getValue();
-        productosActuales.set(position, productoActualizado); // Actualiza el producto en la posición correcta
-        listaProductos.setValue(productosActuales); // Notifica el cambio
+    public void actualizarProducto(int position, ProductoActivity productoActualizado) {
+        List<ProductoActivity> productosActuales = listaProductos.getValue();
+        productosActuales.set(position, productoActualizado);
+        listaProductos.setValue(productosActuales);
     }
 }
 
